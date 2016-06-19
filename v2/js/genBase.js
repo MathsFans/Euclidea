16af9c546f0aba47459dd03cb088f6d7b8e12f9c
Xiaole Tao <xiaolet@cisco.com>
2016-06-19 12:06:39 +0800

    更新基础内容

--- v2/js/genBase.js
+++ v2/js/genBase.js
@@ -8,9 +8,8 @@ for (var packName in packData) {
         var level = pack[index],
             stars = level.stars,
             line = '';
-        contents.push(`##### ${level.id} ${level.title}`);
-        line += `- 解题目标: ${stars[0]}L${stars[1]}E${stars[2]}V `;
-        line += `[查看题目](images/level/${level.image}.png) `;
+        contents.push(`##### ${level.id} ${level.title} 目标: *${stars[0]}L ${stars[1]}E ${stars[2]}V*`);
+        line += `- [查看题目](images/level/${level.image}.png) `;
         if (level.fact) {
             if (level.fact[1]) {
                 line += `[知识点1](images/hints/${level.fact[0]}.png) `;
@@ -27,16 +26,16 @@ for (var packName in packData) {
         }
         contents.push(line);
         if (level.seq) {
-            contents.push(`+ ${stars[0]}L${stars[1]}E步骤：${level.seq}`);
-            contents.push(`- 【待编辑】${stars[0]}L${stars[1]}E解题思路：xxx。[参考图](images/solved/${level.id}.${stars[0]}L${stars[1]}E.png)`);
+            contents.push(`+ *${stars[0]}L${stars[1]}E*步骤：${level.seq}`);
+            contents.push(`- 【待编辑】*${stars[0]}L${stars[1]}E*解题思路：xxx。[参考图](images/solved/${level.id}.${stars[0]}L${stars[1]}E.png)`);
         }
         if (level.lseq) {
-            contents.push(`+ ${stars[0]}L步骤：${level.lseq}`);
-            contents.push(`- 【待编辑】${stars[0]}L${stars[1]}E解题思路：xxx。[参考图](images/solved/${level.id}.${stars[0]}L.png)`);
+            contents.push(`+ *${stars[0]}L*步骤：${level.lseq}`);
+            contents.push(`- 【待编辑】*${stars[0]}L*解题思路：xxx。[参考图](images/solved/${level.id}.${stars[0]}L.png)`);
         }
         if (level.eseq) {
-            contents.push(`+ ${stars[1]}E步骤：${level.eseq}`);
-            contents.push(`- 【待编辑】${stars[0]}L${stars[1]}E解题思路：xxx。[参考图](images/solved/${level.id}.${stars[1]}E.png)`);
+            contents.push(`+ *${stars[1]}E*步骤：${level.eseq}`);
+            contents.push(`- 【待编辑】*${stars[1]}E*解题思路：xxx。[参考图](images/solved/${level.id}.${stars[1]}E.png)`);
         }
         contents.push('\n');
     }
